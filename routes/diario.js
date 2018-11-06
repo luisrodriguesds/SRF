5c41988002628237310259d11b1ed71362ca5bf9
Hyuan Peixoto Farrapo <hyuanpeixoto@gmail.com>
2018-11-06 11:44:01 -0300

    front-end finalizado e back-end iniciado

--- routes/diario.js
+++ routes/diario.js
@@ -7,5 +7,13 @@ router.post('/',function(req,res){
     res.render('diario', {message:'no_msg'});
 });
 
+router.post('/novaTarefa',function(req,res){
+    console.log("Teste para pegar os dados");
+    console.log(req.body.nome);
+    console.log(req.body.fornos);
+    console.log(req.body.titulo);
+    console.log(req.body.descricao);
 
+    
+});
 module.exports = router;
