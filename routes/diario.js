da4e96a4e27f20ee03f27667d1d3f7aa1b202fc0
Hyuan Peixoto Farrapo <hyuanpeixoto@gmail.com>
2018-11-07 12:00:26 -0300

    utilizacao de back-end com ajax

--- routes/diario.js
+++ routes/diario.js
@@ -7,13 +7,42 @@ router.post('/',function(req,res){
     res.render('diario', {message:'no_msg'});
 });
 
+router.get('/',function(req,res){
+    console.log("postou");
+    res.render('diario',{message:'no_msg'});
+});
+
+
 router.post('/novaTarefa',function(req,res){
     console.log("Teste para pegar os dados");
     console.log(req.body.nome);
+    let nome = req.body.nome;
     console.log(req.body.fornos);
+    let forno = req.body.fornos;
     console.log(req.body.titulo);
+    let titulo = req.body.titulo;
     console.log(req.body.descricao);
-
-    
+    let descricao = req.body.descricao;
+    let data = new Date();
+    var db = req.db;
+    var logTarefas = db.get('tarefasLog');
+    var novaTarefa = {
+        "_nomeUsuario" : nome,
+        "_forno" : forno,
+        "_titulo" : titulo,
+        "_descricao" : descricao,
+        "_data" : data
+    }
+    logTarefas.insert(novaTarefa, function(err,r){
+        if(!err){
+            res.send("deu certo!");
+        }
+        else{
+            res.send("erro!");
+            return;
+        }
+    });
 });
+
+
 module.exports = router;
